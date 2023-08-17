72fa3df9dc6ca43740911284c1f9a96af2cfb4be
jose david <81387297+josedavid1996@users.noreply.github.com>
2023-08-17 16:25:42 -0500

    Se cambio la url del backend

--- codegen.ts
+++ codegen.ts
@@ -4,7 +4,7 @@ const config: CodegenConfig = {
   debug: true,
   verbose: true,
   overwrite: true,
-  schema: 'https://de72-190-66-99-88.ngrok-free.app/graphql',
+  schema: 'https://c4a1-190-66-99-88.ngrok-free.app/graphql',
   documents: 'src/**/*.graphql',
   generates: {
     'src/generated/graphql.tsx': {
